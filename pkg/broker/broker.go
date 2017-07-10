fa3208eada3055a475c3ea148be78121b5b9f23e
jesus m. rodriguez <jmrodri@gmail.com>
2017-07-10 09:35:02 -0400

    WIP: auth

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -45,11 +45,17 @@ type Broker interface {
 
 // Config - Configuration for the broker.
 type Config struct {
-	DevBroker          bool `yaml:"dev_broker"`
-	LaunchApbOnBind    bool `yaml:"launch_apb_on_bind"`
-	BootstrapOnStartup bool `yaml:"bootstrap_on_startup"`
-	Recovery           bool `yaml:"recovery"`
-	OutputRequest      bool `yaml:"output_request"`
+	DevBroker          bool       `yaml:"dev_broker"`
+	LaunchApbOnBind    bool       `yaml:"launch_apb_on_bind"`
+	BootstrapOnStartup bool       `yaml:"bootstrap_on_startup"`
+	Recovery           bool       `yaml:"recovery"`
+	OutputRequest      bool       `yaml:"output_request"`
+	Auth               AuthConfig `yaml:"auth"`
+}
+
+type AuthConfig struct {
+	Type    bool `yaml:"type"`
+	Enabled bool `yaml:"enabled"`
 }
 
 // DevBroker - Interface for the development broker.
